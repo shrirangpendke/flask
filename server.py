ae46f6ac5ffb08730ecc99694cab5e94129e0bac
Shrirang Pendke <shrirangpendkes@gmail.com>
2023-07-29 10:03:52 +0530

    srvr

--- server.py
+++ server.py
@@ -3,8 +3,17 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
-
 def root():
-    return "Welcome to third & last version of flask application: v3"
+    return "welcome to ITIL exam"
+
+@app.route("/me", methods=["GET"])
+def root1():
+    return "name: shrirang Pendke, prn: 230344233033, mobile no: 9096701292"
+
+@app.route("/modules", methods=["GET"])
+def root2():
+    return "modules: Networking, Security Concepts, COSA, Complience & Audit, NDC & PKI, Cyber Forensics"
+
+
 
 app.run(host="0.0.0.0", port=4000, debug=True)
